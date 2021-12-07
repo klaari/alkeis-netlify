74a46fcb5b5caa2fb027e494c4d32963d2d395ad
Kari Laari <kari@karin-atk.fi>
2021-12-07 14:10:07 +0200

    Setup Fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,28 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
+
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load('QTSIXZWA', {
+      includedDomains: ['alkeis.com'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />
 }
 
