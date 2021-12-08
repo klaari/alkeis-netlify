e659056b24c0af3761b8eb0657db8718815f4a88
Kari Laari <kari@karin-atk.fi>
2021-12-08 12:49:07 +0200

    Update video

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import React from "react";
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 
+const videoPortrait = '/videos/alkeis_09_1080x1920px.mp4';
+const videoLandscape = '/videos/alkeis_09_1920x1080px.mp4';
+
 export default function Home() {
+
     return (
         <div className="container">
             <Head>
@@ -12,9 +17,42 @@ export default function Home() {
 
             <main>
                 <video autoPlay muted loop playsInline className="video">
-                    <source src="/videos/alkeis_04.mp4" type="video/mp4" />
+                    <source src={videoLandscape} type="video/mp4" />
                 </video>
             </main>
         </div>
     );
+}
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = React.useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  React.useEffect(() => {
+    // only execute all the code below in client side
+    if (typeof window !== 'undefined') {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
